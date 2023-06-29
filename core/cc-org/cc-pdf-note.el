a7037b951d69aaa36808602e9b6266fadb67d1da
chrischen <chrischen@ignity.xyz>
2023-06-29 22:24:03 +0800

    added hint comment

--- core/cc-org/cc-pdf-note.el
+++ core/cc-org/cc-pdf-note.el
@@ -74,7 +74,7 @@
    ("C-c h" . pdf-annot-add-highlight-markup-annotation)
    ("C-c t" . pdf-annot-add-text-annotation)
    ("C-c d" . pdf-annot-delete)
-   ("C-c l" . pdf-annot-list-annotations)
+   ("C-c l" . pdf-annot-list-annotations) ; Hint: <SPC> go to selected annotation
    :map pdf-view-mode-map
    ("d" . 'pdf-view-next-page-command)
    ("a" . 'pdf-view-previous-page-command)
